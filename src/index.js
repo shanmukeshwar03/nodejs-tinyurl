401d55f13fc83a1b69ffd9ec51f459ba467dfcdb
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2021-12-13 22:14:04 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -12,16 +12,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
-
-mongoose.connect(
-  process.env.MONGO_URL,
-  JSON.parse(process.env.MONGO_CONFIG),
-  (err) => {
-    if (err) console.log('Mongodb Connection failed')
-    else console.log('Mongodb Connection succeed!')
-  }
+app.use(
+  cors({ origin: JSON.parse(process.env.ORIGIN).whitelist, credentials: true })
 )
+
+mongoose.connect(process.env.MONGO_URL, JSON.parse(process.env.MONGO_CONFIG))
 
 app.use('/url', Authorise, UrlRouter)
 app.use('/', ShrRouter)
